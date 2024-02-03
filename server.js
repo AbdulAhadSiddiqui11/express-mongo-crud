2b39d8594edd2616bfea380136d3ab2ffbc27dac
AbdulAhadSiddiqui11 <abdulahadsiddiqui011@gmail.com>
2024-02-03 22:11:22 +0530

    Mongo DB and Mongoose conection

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 import express from "express";
+import colors from "colors";
 import { config } from "dotenv";
 import { errorHandler } from "./middleware/errorMiddleware.js";
+import connectDB from "./config/db.js";
 import goalsRouter from "./routes/goalRoutes.js";
 
 const dotenv = config();
 
 const port = process.env.PORT || 3000;
+
+connectDB();
 
 const app = express();
 app.use(express.json());
