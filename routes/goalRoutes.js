8eb9003c073af8044164088cfeb8e5899efcd0d8
AbdulAhadSiddiqui11 <abdulahadsiddiqui011@gmail.com>
2024-02-03 21:33:58 +0530

    Cleaning routes

--- routes/goalRoutes.js
+++ routes/goalRoutes.js
@@ -8,16 +8,12 @@ import {
 
 const goalsRouter = express.Router();
 
-// goalsRouter.get("/", getGoals);
-
-// goalsRouter.post("/", setGoal);
-
 goalsRouter.route("/").get(getGoals).post(setGoal);
-
-// goalsRouter.put("/:id", updateGoal);
-
-// goalsRouter.delete("/:id", deleteGoal);
-
 goalsRouter.route("/:id").put(updateGoal).delete(deleteGoal);
 
 export default goalsRouter;
+
+// goalsRouter.put("/:id", updateGoal);
+// goalsRouter.delete("/:id", deleteGoal);
+// goalsRouter.get("/", getGoals);
+// goalsRouter.post("/", setGoal);
